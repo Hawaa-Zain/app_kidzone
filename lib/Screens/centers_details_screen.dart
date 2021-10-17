e118cf808ec3d011af6b57f0ef6c1d7407d2ee47
Hawaahh <Hawaa.tz@gmail.com>
2021-10-18 01:26:41 +0300

    kidzoneGit

--- lib/Screens/centers_details_screen.dart
+++ lib/Screens/centers_details_screen.dart
@@ -7,7 +7,7 @@ class CentersDetailScreen extends StatelessWidget {
   //late final String title;
   // CentersDetailScreen(this.title);
   static String routName = '/centers-detail';
-
+  String read =' تستقبل الحضانة الأطفال من عمر ثلاث أشهر إلى ست سنوات ولديهم باقات تشمل رعاية نهارية فقط ,وباقات تشمل رعاية نهارية بالإضافة إلى الأكل وتغيير الحفاضات ,وباقة العناية تشمل رعاية نهارية بالإضافة إلى الأكل وتغيير الحفاضات ومراقبة الطفل في أي وقت وايضا رعاية مسائية خلال عطلات نهاية الأسبوع. ';
   @override
   Widget build(BuildContext context) {
     final centersTitle = ModalRoute
@@ -31,26 +31,40 @@ class CentersDetailScreen extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-          Container(
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(10),
-            alignment: Alignment.bottomCenter,
-            child: Text("معلومات الإتصال"),
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(10),
-            alignment: Alignment.bottomCenter,
-            child: Text(
-              "التقييم",
+          SizedBox(height: 10),
+          SingleChildScrollView(
+            child: Container(
+              child: Text(read,
+              style: TextStyle(
+                color: Colors.purple.shade500,
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),),
             ),
           ),
           Container(
             margin: EdgeInsets.all(10),
             padding: EdgeInsets.all(10),
             alignment: Alignment.bottomCenter,
-            child: Text("الموقع"),
+            child: Text("موقع الحضانة: جدة حي الزهراء",
+            style: TextStyle(
+              color: Colors.purple.shade500,
+              fontWeight: FontWeight.bold,
+              fontSize: 10,
+            ),),
           ),
+      Container(
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        alignment: Alignment.bottomCenter,
+        child: Text("للتواصل والإستفسار على الرقم: 0544699331",
+          style: TextStyle(
+            color: Colors.purple.shade500,
+            fontWeight: FontWeight.bold,
+            fontSize: 10,
+          ),
+        ),
+      ),
           SizedBox(height: 20),
           Center(
             child: Column(
