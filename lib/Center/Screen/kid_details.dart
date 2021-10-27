1fc0e9da12de7f04405c281ae23ec44659ef9c0e
Dalal BinMahfouz <idalal.bm@gmail.com>
2021-10-27 03:50:56 +0300

    'اخر تحديث'

--- lib/Center/Screen/kid_details.dart
+++ lib/Center/Screen/kid_details.dart
@@ -1,15 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:kidzone_app/Center/widget/Kids_grid.dart';
 
 class KidsDetails extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('معلومات الطفل'),
-        backgroundColor: Colors.purple[300],
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text('معلومات الطفل'),
+      backgroundColor: Colors.purple[300],
+    ),
+    body: Container(
+        alignment: Alignment.topCenter,
+        child: Card(
+          margin: EdgeInsets.all(15),
+          color: Colors.white,
+          shadowColor: Colors.black,
+          elevation: 30,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(
+                  Icons.group_outlined,
+                  color: Colors.purple[300],
+                  size: 30,
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.date_range_outlined,
+                  color: Colors.purple[300],
+                  size: 30,
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.purple[300],
+                  size: 30,
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.assignment_turned_in_outlined   ,
+                  color: Colors.purple,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: KidsGrid(),
-    );
+      );
   }
-}
+
