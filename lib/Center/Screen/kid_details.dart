d919d48493d209d75d69a3f9c8424f04e6ff9044
Dalal BinMahfouz <idalal.bm@gmail.com>
2021-10-28 00:32:11 +0300

    'صفحة الاعلانات /و التسجيل في الحضانة'

--- lib/Center/Screen/kid_details.dart
+++ lib/Center/Screen/kid_details.dart
@@ -2,15 +2,47 @@ import 'package:flutter/material.dart';
 
 class KidsDetails extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: Text('معلومات الطفل'),
-      backgroundColor: Colors.purple[300],
-    ),
-    body: Container(
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Column(children: [
+      Container(
+        height: 200,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(50),
+              bottomRight: Radius.circular(50)),
+          color: new Color(0xFFFFFFFF),
+          gradient: LinearGradient(
+            colors: [(new Color(0xFFBBA68C8)), new Color(0xFFBBA68C8)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(right: 20, top: 20),
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                "معلومات الطفل ",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        )),
+      ),
+      Container(
         alignment: Alignment.topCenter,
         child: Card(
-          margin: EdgeInsets.all(15),
+          margin: EdgeInsets.all(30),
           color: Colors.white,
           shadowColor: Colors.black,
           elevation: 30,
@@ -40,7 +72,7 @@ class KidsDetails extends StatelessWidget {
               ),
               ListTile(
                 leading: Icon(
-                  Icons.assignment_turned_in_outlined   ,
+                  Icons.assignment_turned_in_outlined,
                   color: Colors.purple,
                   size: 30,
                 ),
@@ -49,6 +81,6 @@ class KidsDetails extends StatelessWidget {
           ),
         ),
       ),
-      );
+    ])));
   }
-
+}
--- lib/Parent/register_screen.dart
+++ lib/Parent/register_screen.dart
@@ -5,90 +5,123 @@ import 'package:kidzone_app/Parent/splash_screen.dart';
 void main() => runApp(RegisterScreen());
 
 class RegisterScreen extends StatelessWidget {
-  get image => null;
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text('التسجيل في الحضانة '),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8),
+  Widget build(BuildContext context) {
+  return Scaffold(
+      body: SingleChildScrollView(
           child: Column(children: [
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "اسم الطفل الثلاثي...",
-                labelText: "الإسم:",
-                labelStyle:
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50)),
+                color: new Color(0xFFFFFFFF),
+                gradient: LinearGradient(
+                  colors: [(new Color(0xFFBBA68C8)), new Color(0xFFBBA68C8)],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 20, top: 20),
+                        alignment: Alignment.bottomCenter,
+                        child: Text(
+                          " التسجيل في الحضانة ",
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "اسم الطفل الثلاثي...",
+                    labelText: "الإسم:",
+                    labelStyle:
                     TextStyle(fontSize: 15, color: Colors.purple.shade300),
-                border: OutlineInputBorder(),
-                prefixIcon:
+                    border: OutlineInputBorder(),
+                    prefixIcon:
                     Icon(Icons.child_care, color: Colors.purple.shade300),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "شهر/سنة...",
-                labelText: "العمر:",
-                labelStyle:
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "شهر/سنة...",
+                    labelText: "العمر:",
+                    labelStyle:
                     TextStyle(fontSize: 25, color: Colors.purple.shade300),
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.date_range_outlined,
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.date_range_outlined,
                     color: Colors.purple.shade300),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "رقم ولي أمر الطفل",
-                labelText: "التواصل:",
-                labelStyle:
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "رقم ولي أمر الطفل",
+                    labelText: "التواصل:",
+                    labelStyle:
                     TextStyle(fontSize: 15, color: Colors.purple.shade300),
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.phone, color: Colors.purple.shade300),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: " ساعة/يومي/شهر/ترم؟.....",
-                labelText: "التسجيل:",
-                labelStyle:
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.phone, color: Colors.purple.shade300),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: " ساعة/يومي/شهر/ترم؟.....",
+                    labelText: "التسجيل:",
+                    labelStyle:
                     TextStyle(fontSize: 15, color: Colors.purple.shade300),
-                border: OutlineInputBorder(),
-                prefixIcon:
+                    border: OutlineInputBorder(),
+                    prefixIcon:
                     Icon(Icons.app_registration, color: Colors.purple.shade300),
-              ),
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
+                  ),
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      RaisedButton(
+                        onPressed: () {
+                          Navigator.push(
                         context,
                         MaterialPageRoute(builder: (context) => SplashScreen()),
-                      );
-                    },
-                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 80),
-                    color: Colors.purple.shade300,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                    child: Text(
-                      'إرسال طلب',
-                      style: TextStyle(
-                        color: Colors.white,
+                          );
+                          },
+                        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 80),
+                        color: Colors.purple.shade300,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                        ),
+                        child: Text(
+                          'إرسال طلب',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ]),
             ),
-          ]),
-        ),
-      );
+          ])));
+  }
 }
